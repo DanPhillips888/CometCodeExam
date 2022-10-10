5f880009669a2fc0b4406527f5191c52a698de86
Dan Phillips <dan.phillips888@gmail.com>
2022-10-11 11:18:09 +1300

    Input added
    Added input fields which adjust the efficiency score output

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ var scoreStopSign = 0;
 var scoreTrafficLight = 0;
 
 // score scale 1 to 100 bad to good
-// function which control scores highest?
+// function which control scores highest
+
 // roundabout
 const roundabout = (cpm) => {
     if (cpm < 10) {
@@ -25,12 +26,12 @@ const roundabout = (cpm) => {
         scoreRoundabout += 75;
     }
     else {
-        scoreRoundabout += 90;
+        scoreRoundabout += 60;
     }
     return scoreRoundabout;
 }
 roundabout(cpm);
-console.log("roundabout ",scoreRoundabout);
+console.log("roundabout: ",scoreRoundabout);
 
 // stop sign
 const stopSign = (cpm) => {
@@ -46,7 +47,7 @@ const stopSign = (cpm) => {
     return scoreStopSign;
 }
 stopSign(cpm);
-console.log("stopsign", scoreStopSign);
+console.log("stopsign: ", scoreStopSign);
 
 // traffic light
 const trafficLight = (cpm) => {
@@ -64,23 +65,92 @@ const trafficLight = (cpm) => {
 trafficLight(cpm);
 console.log("trafficLight: ", scoreTrafficLight);
 
+// reset scores function
+const reset = () => {
+    scoreRoundabout = 0;
+    scoreStopSign = 0;
+    scoreTrafficLight = 0;
+    cpm = northCPM + eastCPM + southCPM + westCPM;
+    console.log(cpm);
+    roundabout(cpm);
+    stopSign(cpm);
+    trafficLight(cpm);
+    const updateScoreDisplay = () => {
+        let roundabout = document.querySelector(".roundabout");
+        roundabout.querySelector("span").innerHTML = `${scoreRoundabout}`;
+        let stopSign = document.querySelector(".stopSign");
+        stopSign.querySelector("span").innerHTML = `${scoreStopSign}`;
+        let trafficLight = document.querySelector(".trafficLight");
+        trafficLight.querySelector("span").innerHTML = `${scoreTrafficLight}`;
+    };
+    updateScoreDisplay();
+
+    console.log("roundabout: ",scoreRoundabout);
+    console.log("stopsign: ", scoreStopSign);
+    console.log("trafficLight: ", scoreTrafficLight);
+}
+
+// Input component
+const inputUI = (roadsArray) => {
+    roadsArray.forEach((listElement) => {
+        
+        let direction = listElement.getAttribute("road-direction");
+        const inputForm = document.createElement("form");
+        inputForm.classList.add(`${direction}`);
+    
+        inputForm.innerHTML = `
+            <input type="number" roadID="${direction}" placeholder="Enter New ${direction} Rate">
+            <button>Update</button>
+        `;
+    
+        inputForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let newValue = inputForm.querySelector("input").value;
+
+            if (direction === "north"){
+                northCPM = Number(newValue);
+                console.log(`${direction}CPM: `, northCPM);
+            } else if (direction === "east"){
+                eastCPM = Number(newValue);
+                console.log(`${direction}CPM: `, eastCPM);
+            } else if (direction === "south"){
+                southCPM = Number(newValue);
+                console.log(`${direction}CPM: `, southCPM);
+            } else {
+                westCPM = Number(newValue);
+                console.log(`${direction}CPM: `, westCPM);
+            }
+            // this line only changes the html not the road rate variable
+            listElement.querySelector("span").innerHTML = `${newValue}`;
+            inputForm.querySelector("input").value = "";
+            reset();
+        });
+        listElement.append(inputForm);
+    });
+};
+
+// UI display HTML
 const efficiencyScore = () => {
     let efficiency = document.createElement("article");
     
     efficiency.innerHTML = `
     <ul class="carsPerMinuteRate">
-      <li>North Road Rate: ${northCPM}</li>
-      <li>East Road Rate: ${eastCPM}</li>
-      <li>South Road Rate: ${southCPM}</li>
-      <li>West Road Rate: ${westCPM}</li>
+      <li class="roadRate" road-direction="north">North Road Rate: <span>${northCPM}</span></li>
+      <li class="roadRate" road-direction="east">East Road Rate: <span>${eastCPM}</span></li>
+      <li class="roadRate" road-direction="south">South Road Rate: <span>${southCPM}</span></li>
+      <li class="roadRate" road-direction="west">West Road Rate: <span>${westCPM}</span></li>
     </ul>
-    <h1 class="efficiencyScore">Efficiency Score:</h1>
+    <h1 class="efficiencyScoreTitle">Efficiency Score:</h1>
     <ul class="controlType">
-      <li>Roundabout: ${scoreRoundabout}%</li>
-      <li>Stop Sign: ${scoreStopSign}%</li>
-      <li>Traffic Lights: ${scoreTrafficLight}%</li>
+      <li class="roundabout">Roundabout: <span>${scoreRoundabout}</span>%</li>
+      <li class="stopSign">Stop Sign: <span>${scoreStopSign}</span>%</li>
+      <li class="trafficLight">Traffic Lights: <span>${scoreTrafficLight}</span>%</li>
     </ul>
     `;
+
+    let roadCPMRate = efficiency.querySelectorAll(".roadRate");
+    inputUI(roadCPMRate);
+
     return efficiency;
 }
 
